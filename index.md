5da4bd313c1c8c9a762da1d51929c5749528777b
agilbudi <agilbudiprasetyo@gmail.com>
2021-08-21 00:17:55 +0700

    create first page

--- index.md
+++
@@ -1,2 +0,0 @@
-# WAMIKA Developer
-## Assalamualaikum
---
+++ js/index.js
@@ -0,0 +1,13 @@
+let gradientify = new Gradientify(
+    '#background',
+    [
+      "linear-gradient(60deg, #58E605, #FEEC00)", // Array of CSS gradients
+      "linear-gradient(10deg, #03F993, #58E605)",
+      "radial-gradient(#ABFFE7, #58E605)",
+      "linear-gradient(60deg, #58E605, #03F993)",
+      "radial-gradient(7deg, #FEEC00, #58E605)"
+    ],
+    3200 // Fading interval in miliseconds
+)
+
+//58E605

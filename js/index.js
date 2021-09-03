6189d3c09abe92380052d82d4a53eb80c95efef8
agilbudi <agilbudiprasetyo@gmail.com>
2021-09-03 11:00:00 +0700

    update and add page

--- js/index.js
+++ js/index.js
@@ -1,13 +1,19 @@
-let gradientify = new Gradientify(
-    '#background',
-    [
-      "linear-gradient(60deg, #58E605, #FEEC00)", // Array of CSS gradients
-      "linear-gradient(10deg, #03F993, #58E605)",
-      "radial-gradient(#ABFFE7, #58E605)",
-      "linear-gradient(60deg, #58E605, #03F993)",
-      "radial-gradient(7deg, #FEEC00, #58E605)"
-    ],
-    3500 // Fading interval in miliseconds
-);
+function bgAnimation(object) {
+    let gradientify = new Gradientify(
+      object,
+      [
+        "linear-gradient(60deg, #58E605, #FEEC00)", // Array of CSS gradients
+        "linear-gradient(10deg, #03F993, #58E605)",
+        "radial-gradient(#ABFFE7, #58E605)",
+        "linear-gradient(60deg, #58E605, #03F993)",
+        "radial-gradient(7deg, #FEEC00, #58E605)"
+      ],
+      3500 // Fading interval in miliseconds
+    );
+    return gradientify;
+}
+
+bgAnimation('#background');
+bgAnimation('#bg-isi');
 
 //58E605
